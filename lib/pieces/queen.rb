59c00ec68486000fa86253a7272345ba52f297b7
Aliyu <biggy0023@gmail.com>
2023-02-17 22:01:45 +0100

    Done some justice to the queen piece!

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -5,8 +5,45 @@ require_relative 'pieces'
 # queen class
 class Queen < ChessPiece
   def possible_moves
-    # king moves in any number of squares in any direction
+    # queen moves in any number of squares in any direction
     [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
+  end
+
+  def move(board, row, col)
+    row1, col1 = position
+    row2 = row
+    col2 = col
+    delta_x = (row1 - row2).abs
+    delta_y = (col1 - col2).abs
+    if delta_x.zero? || delta_y.zero? || delta_x == delta_y && board.in_bound?(row2, col2)
+      if obstructed?(board, row1, col1, row2, col2) == false
+        update_position(row2, col2)
+        board.grid[row2][col2] = self
+        true
+      else
+        puts 'Path is obstructed'
+        false
+      end
+    else
+      puts 'Invalid move'
+      false
+    end
+  end
+
+  def obstructed?(board, row1, col1, row2, col2)
+    x_dir = col2 <=> col1
+    y_dir = row2 <=> row1
+
+    row = row1 + y_dir
+    col = col1 + x_dir
+
+    while [row, col] != [row2, col2]
+      return true if board.grid[row][col] != '-'
+
+      row += y_dir
+      col += x_dir
+    end
+    false
   end
 
   def to_s
@@ -16,7 +53,7 @@ class Queen < ChessPiece
     when 'black'
       "\u{265B}"
     else
-      puts "#{color} not valid"
+      raise "#{color} not valid"
     end
   end
 end
--- main.rb
+++ main.rb
@@ -5,3 +5,12 @@ require_relative 'lib/pieces/king'
 require_relative 'lib/pieces/queen'
 require_relative 'lib/pieces/bishop'
 require_relative 'lib/player'
+
+board = Board.new
+king = King.new('white')
+king.update_position(0, 4)
+queen = Queen.new('black')
+queen.update_position(7, 5)
+king.move(board, 1, 4)
+queen.move(board, 6, 5)
+board.layout
