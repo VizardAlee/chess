2fed200d6724fcc86344103151384dcaec1676b6
Aliyu <biggy0023@gmail.com>
2023-02-24 16:42:27 +0100

    As for the game class, we set up the board along with the pieces designated to their respective positions. And as for player class, all pieces have to be represented

--- lib/game.rb
+++ lib/game.rb
@@ -40,12 +40,88 @@ class Game
       @player1 = Player.new(player2_name, player2_color)
       @player2 = Player.new(player1_name, player1_color)
     end
-
+    board_setup_black
+    board_setup_white
     declaration
   end
 
   def declaration
     puts 'Player with the white army strikes first!'
     puts "#{player1.name} VS #{player2.name}! Let's Goo!!"
+    show
+  end
+
+  def board_setup_white
+    board.grid[7][0] = player1.rook1
+    player1.rook1.update_position(7, 0)
+    board.grid[7][1] = player1.knight1
+    player1.knight1.update_position(7, 1)
+    board.grid[7][2] = player1.bishop1
+    player1.bishop1.update_position(7, 2)
+    board.grid[7][3] = player1.queen
+    player1.queen.update_position(7, 3)
+    board.grid[7][4] = player1.king
+    player1.king.update_position(7, 4)
+    board.grid[7][5] = player1.bishop2
+    player1.bishop2.update_position(7, 5)
+    board.grid[7][6] = player1.knight2
+    player1.knight2.update_position(7, 6)
+    board.grid[7][7] = player1.rook2
+    player1.rook2.update_position(6, 7)
+    board.grid[6][0] = player1.pawn1
+    player1.pawn1.update_position(6, 0)
+    board.grid[6][1] = player1.pawn2
+    player1.pawn2.update_position(6, 1)
+    board.grid[6][2] = player1.pawn3
+    player1.pawn3.update_position(6, 2)
+    board.grid[6][3] = player1.pawn4
+    player1.pawn4.update_position(6, 3)
+    board.grid[6][4] = player1.pawn5
+    player1.pawn5.update_position(6, 4)
+    board.grid[6][5] = player1.pawn6
+    player1.pawn6.update_position(6, 5)
+    board.grid[6][6] = player1.pawn7
+    player1.pawn7.update_position(6, 6)
+    board.grid[6][7] = player1.pawn8
+    player1.pawn8.update_position(6, 7)
+  end
+
+  def board_setup_black
+    board.grid[0][0] = player2.rook1
+    player2.rook1.update_position(0, 0)
+    board.grid[0][1] = player2.knight1
+    player2.knight1.update_position(0, 1)
+    board.grid[0][2] = player2.bishop1
+    player2.bishop1.update_position(0, 2)
+    board.grid[0][3] = player2.queen
+    player2.queen.update_position(0, 3)
+    board.grid[0][4] = player2.king
+    player2.king.update_position(0, 4)
+    board.grid[0][5] = player2.bishop2
+    player2.bishop2.update_position(0, 5)
+    board.grid[0][6] = player2.knight2
+    player2.knight2.update_position(0, 6)
+    board.grid[0][7] = player2.rook2
+    player2.rook2.update_position(0, 7)
+    board.grid[1][0] = player2.pawn1
+    player2.pawn1.update_position(1, 0)
+    board.grid[1][1] = player2.pawn2
+    player2.pawn2.update_position(1, 1)
+    board.grid[1][2] = player2.pawn3
+    player2.pawn3.update_position(1, 2)
+    board.grid[1][3] = player2.pawn4
+    player2.pawn4.update_position(1, 3)
+    board.grid[1][4] = player2.pawn5
+    player2.pawn5.update_position(1, 4)
+    board.grid[1][5] = player2.pawn6
+    player2.pawn6.update_position(1, 5)
+    board.grid[1][6] = player2.pawn7
+    player2.pawn7.update_position(1, 6)
+    board.grid[1][7] = player2.pawn8
+    player2.pawn8.update_position(1, 7)
+  end
+
+  def show
+    board.layout
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -10,12 +10,12 @@ require_relative './pieces/pawn'
 # create player
 class Player
   attr_reader :name, :pieces
-  attr_accessor :king, :queen, :rook, :bishop, :knight, :pawn
+  attr_accessor :king, :queen, :rook1, :rook2, :bishop1, :bishop2, :knight1, :knight2, :pawn1, :pawn2, :pawn3, :pawn4, :pawn5, :pawn6, :pawn7, :pawn8
 
   def initialize(name, color)
     @name = name
     @color = choose_piece(color)
-    @pieces = %i[king queen rook bishop knight pawn]
+    @pieces = %i[king queen rook1 rook2 bishop1 bishop2 knight1 knight2 pawn1 pawn2 pawn3 pawn4 pawn5 pawn6 pawn7 pawn8]
   end
 
   def choose_piece(color)
@@ -32,18 +32,38 @@ class Player
   def initiate_whites
     @king = King.new('white')
     @queen = Queen.new('white')
-    @rook = Rook.new('white')
-    @bishop = Bishop.new('white')
-    @knight = Knight.new('white')
-    @pawn = Pawn.new('white')
+    @rook1 = Rook.new('white')
+    @rook2 = Rook.new('white')
+    @bishop1 = Bishop.new('white')
+    @bishop2 = Bishop.new('white')
+    @knight1 = Knight.new('white')
+    @knight2 = Knight.new('white')
+    @pawn1 = Pawn.new('white')
+    @pawn2 = Pawn.new('white')
+    @pawn3 = Pawn.new('white')
+    @pawn4 = Pawn.new('white')
+    @pawn5 = Pawn.new('white')
+    @pawn6 = Pawn.new('white')
+    @pawn7 = Pawn.new('white')
+    @pawn8 = Pawn.new('white')
   end
 
   def initiate_blacks
     @king = King.new('black')
     @queen = Queen.new('black')
-    @rook = Rook.new('black')
-    @bishop = Bishop.new('black')
-    @knight = Knight.new('black')
-    @pawn = Pawn.new('black')
+    @rook1 = Rook.new('black')
+    @rook2 = Rook.new('black')
+    @bishop1 = Bishop.new('black')
+    @bishop2 = Bishop.new('black')
+    @knight1 = Knight.new('black')
+    @knight2 = Knight.new('black')
+    @pawn1 = Pawn.new('black')
+    @pawn2 = Pawn.new('black')
+    @pawn3 = Pawn.new('black')
+    @pawn4 = Pawn.new('black')
+    @pawn5 = Pawn.new('black')
+    @pawn6 = Pawn.new('black')
+    @pawn7 = Pawn.new('black')
+    @pawn8 = Pawn.new('black')
   end
 end
