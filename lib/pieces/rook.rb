6df7c3e705c3277e18ebf2e4162f019b6e613dbc
Aliyu <biggy0023@gmail.com>
2023-03-15 17:34:52 +0100

    completed rook too

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -4,9 +4,33 @@ require_relative 'pieces'
 
 # rook class
 class Rook < ChessPiece
-  def possible_moves
-    # rook moves in any number of squares in vertically or horizontally
-    [[-1, 0], [0, 1], [1, 0], [0, -1]]
+  def move_piece(board, row, col)
+    in_row, in_col = @position
+    delta_x = (in_row - row).abs
+    delta_y = (in_col - col).abs
+
+    if board.in_bound?(row, col)
+      if check_opposition(board, row, col) == true
+        move_condition(board, row, col, delta_x, delta_y)
+      else
+        puts 'That is you comrade'
+        false
+      end
+    else
+      puts 'Out of bounds'
+      false
+    end
+  end
+
+  def move_condition(board, row, col, delta_x, delta_y)
+    in_row, in_col = position
+    if (delta_x.zero? && delta_y >= 1) || (delta_x >= 1 && delta_y.zero?) && obstructed?(board, in_row, in_col, row, col, delta_x, delta_y) == false
+      move(board, row, col)
+      board.grid[in_row][in_col] = '-'
+    else
+      puts 'Invalid move'
+      false
+    end
   end
 
   def to_s
--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ require_relative 'lib/pieces/bishop'
 require_relative 'lib/pieces/queen'
 require_relative 'lib/pieces/knight'
 require_relative 'lib/pieces/pawn'
+require_relative 'lib/pieces/rook'
 
 board = Board.new
 king = King.new('white')
@@ -14,6 +15,7 @@ bqueen = Queen.new('black')
 bishop = Bishop.new('white')
 knight = Knight.new('black')
 pawn = Pawn.new('white')
+rook = Rook.new('black')
 king.move(board, 4, 5)
 king.move_piece(board, 5, 5)
 queen.move(board, 0, 0)
@@ -26,5 +28,8 @@ knight.move(board, 7, 6)
 knight.move_piece(board, 6, 4)
 pawn.move(board, 6, 6)
 pawn.move_piece(board, 5, 5)
+rook.move(board, 7, 3)
+rook.move_piece(board, 3, 3)
+queen.move_piece(board, 3, 3)
 # bishop.move_piece(board, 4, 4)
 board.layout
