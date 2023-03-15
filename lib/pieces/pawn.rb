49cc223e36a4c9390a6a599a9a5720f1a08d749d
Aliyu <biggy0023@gmail.com>
2023-03-15 17:24:11 +0100

    completed the pawn piece

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -5,7 +5,51 @@ require_relative 'pieces'
 # pawn class
 class Pawn < ChessPiece
   def move_piece(board, row, col)
-    
+    in_row, in_col = @position
+    delta_x = (in_row - row).abs
+    delta_y = (in_col - col).abs
+
+    if board.in_bound?(row, col)
+      if check_opposition(board, row, col) == true
+        move_condition(board, row, col, delta_x, delta_y)
+      else
+        puts 'That is you comrade'
+        false
+      end
+    else
+      puts 'Out of bounds'
+      false
+    end
+  end
+
+  def move_condition(board, row, col, delta_x, delta_y)
+    in_row, in_col = position
+
+    if takeable?(board, delta_x, delta_y, row, col)
+      move(board, row, col)
+      board.grid[in_row][in_col] = '-'
+    elsif in_row == 1 && color == 'black' || in_row == 6 && color == 'white'
+      if (delta_x == 1 && delta_y.zero?) || (delta_x == 2 && delta_y.zero?)
+        move(board, row, col)
+        board.grid[in_row][in_col] = '-'
+      end
+    elsif delta_x == 1
+      if board.grid[row][col] == '-'
+        move(board, row, col)
+        board.grid[in_row][in_col] = '-'
+      else
+        puts 'Invalid move'
+        false
+      end
+    end
+  end
+
+  def takeable?(board, delta_x, delta_y, row, col)
+    if ((delta_x - delta_y).zero? && board.grid[row][col] != color) || ((delta_x - delta_y).abs.zero? && board.grid[row][col] != '-')
+      true
+    else
+      false
+    end
   end
 
   def to_s
--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require_relative 'lib/pieces/king'
 require_relative 'lib/pieces/bishop'
 require_relative 'lib/pieces/queen'
 require_relative 'lib/pieces/knight'
+require_relative 'lib/pieces/pawn'
 
 board = Board.new
 king = King.new('white')
@@ -12,6 +13,7 @@ queen = Queen.new('white')
 bqueen = Queen.new('black')
 bishop = Bishop.new('white')
 knight = Knight.new('black')
+pawn = Pawn.new('white')
 king.move(board, 4, 5)
 king.move_piece(board, 5, 5)
 queen.move(board, 0, 0)
@@ -21,6 +23,8 @@ bqueen.move_piece(board, 5, 5)
 bishop.move(board, 0, 6)
 bishop.move_piece(board, 3, 3)
 knight.move(board, 7, 6)
-knight.move_piece(board, 5, 5)
+knight.move_piece(board, 6, 4)
+pawn.move(board, 6, 6)
+pawn.move_piece(board, 5, 5)
 # bishop.move_piece(board, 4, 4)
 board.layout
